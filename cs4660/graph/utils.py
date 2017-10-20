f9cde9038478f8a50f56c0c47d54f40a5b2da0ca
hippiewho <hippiewho@yahoo.com>
2017-10-19 22:12:02 -0700

    Started Utils

--- cs4660/graph/utils.py
+++ cs4660/graph/utils.py
@@ -3,6 +3,8 @@ utils package is for some quick utility methods
 
 such as parsing
 """
+import graph.graph
+
 
 class Tile(object):
     """Node represents basic unit of graph"""
@@ -39,37 +41,44 @@ def parse_grid_file(graph, file_path):
 
     # TODO: for each node/edge above, add it to graph
     file = open(file_path)
-    text = file.read()
-    lines = text.split('\n')
-    grid = []
-    for line in lines:
-        for i in range (1,len(line) - 2,2):
-            grid.append(line[i:i+2])
-
-    grid = grid[1:-1]        
+    lines = file.read().split('\n')[1:-2]
     
-    print grid
+    count = 0
+    grid = []
+
+    count = 0
+
+    for i in lines:
+        row = []
+        for j in range(1, len(i) - 2 , 2):
+            row.append(i[j:j + 2])
+        lines[count] = row
+        count += 1
+
+    grid = lines
+
+    for s in grid:
+        print s
 
     tiles = {}
 
     for Y in range(len(grid)):
         for x in range (len(grid[0])):
             tile = Tile(x, y, grid[y][x])
+            tiles[(x, y)] = tile           
             graph.add_node(Node(tile))
-            tiles[(x, y)] = tile
 
     for Y in range(len(grid)):
         for x in range (len(grid[0])):
             current_tile = Tile(x, y, grid[y][x])
 
             if current_tile.symbol == "##":
-                continue #obstacle tile not suppose to have edge
+                continue
 
-            #check that adjacent tiles are valid, order is N -> E -> W -> S
-            
             if (x, y - 1) in tiles:
                 upper_tile = tiles[(x, y - 1)]
                 if upper_tile.symbol != "##":
+                    print Edge(Node(current_tile), Node(upper_tile), 1)
                     graph.add_edge(Edge(Node(current_tile), Node(upper_tile), 1))   
 
             if (x + 1, y) in tiles:
@@ -97,3 +106,5 @@ def convert_edge_to_grid_actions(edges):
     """
     return ""
 
+# grid_4_path = '../test/fixtures/grid-4.txt'
+# graph_4 = parse_grid_file(graph.AdjacencyList(), grid_4_path)
